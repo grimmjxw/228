e5377b4195fceb2349eb54065d523264ada1e58d
grimmjxw <grimmjow230704@gmail.com>
2024-06-09 05:24:58 +0300

    Разрешен конфликт в функции main(FileName)

--- WeatherService/FileName.cpp
+++ WeatherService/FileName.cpp
@@ -1,6 +1,10 @@
 #include "XmlService.h"
+#include "JsonService.h"
 int main() {
+	JsonService js;
+	Weather w1 = js.getWeather("weather.json");
+	
 	XmlService xs;
-	Weather w = xs.getWeather("weather.xml");
+	Weather w2 = xs.getWeather("weather.xml");
 	return 0;
 }
